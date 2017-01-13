0eb0ef278dd1b8ecc24cf84cea7df4e74ccd8918
Cody Pearce <codyapearce@gmail.com>
2017-01-13 07:04:33 -0700

    move everything into codeground function

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-var htmlEditor = document.querySelector('#html textarea'),
-    cssEditor = document.querySelector('#css textarea'),
-    jsEditor = document.querySelector('#js textarea');
+
 
 function Codeground() {
     var codeground = document.getElementById('codeground');
@@ -32,78 +30,84 @@ function Codeground() {
         code.appendChild(textarea);
     }
 
+    function keyupRender() {
+        var editors = [htmlEditor, cssEditor, jsEditor];
+
+        editors.forEach(function(editor, i, arr) {
+           editor.addEventListener('keyup', function() {
+               render();
+           }, false);
+        });
+
+    }
+
+
     // Create Editors
     createEditor('html', editorsDiv);
     createEditor('css', editorsDiv);
     createEditor('js', editorsDiv);
 
+    var htmlEditor = document.querySelector('#html textarea'),
+        cssEditor = document.querySelector('#css textarea'),
+        jsEditor = document.querySelector('#js textarea');
+
     var iframe = document.createElement('iframe');
     outputDiv.appendChild(iframe);
 
+
+
+
+
+    function prepareSource() {
+        var html = htmlEditor.value,
+            css = cssEditor.value,
+            js = jsEditor.value,
+            src = '';
+
+        var baseTemplate =
+            "<!doctype html>\n" +
+            "<html>\n\t" +
+            "<head>\n\t\t" +
+            "<meta charset=\"utf-8\">\n\t\t" +
+            "<title>Test</title>\n\n\t\t\n\t" +
+            "</head>\n\t" +
+            "<body>\n\t\n\t" +
+            "</body>\n" +
+            "</html>";
+
+        src = baseTemplate.replace('</body>', html + '</body>');
+
+        css = '<style>' + css + '</style>';
+        src = src.replace('</head>', css + '</head>');
+
+        js = '<script>' + js + '</script>';
+        src = src.replace('</body>', js + '</body>');
+
+        return src;
+    };
+    function render() {
+        var source = prepareSource();
+
+        var iframe = document.querySelector('.output iframe'),
+            iframe_doc = iframe.contentDocument;
+
+        iframe_doc.open();
+        iframe_doc.write(source);
+        iframe_doc.close();
+    };
+
+    function preset(presetHTML, presetCSS, presetJS) {
+        if(presetHTML)
+            htmlEditor.value += presetHTML;
+        if(presetCSS)
+            cssEditor.value += presetCSS;
+        if(presetJS)
+            jsEditor.value += presetJS;
+        render();
+    }
+    
     keyupRender();
     preset('<h1>test</h1>', 'h1{color:red}', 'console.log("test")');
 
 }
 var codeground = new Codeground();
-
-
-function keyupRender() {
-    var editors = [htmlEditor, cssEditor, jsEditor];
-
-    editors.forEach(function(editor, i, arr) {
-       editor.addEventListener('keyup', function() {
-           render();
-       }, false);
-    });
-
-}
-
-
-
-function prepareSource() {
-    var html = htmlEditor.value,
-        css = cssEditor.value,
-        js = jsEditor.value,
-        src = '';
-
-    var baseTemplate =
-        "<!doctype html>\n" +
-        "<html>\n\t" +
-        "<head>\n\t\t" +
-        "<meta charset=\"utf-8\">\n\t\t" +
-        "<title>Test</title>\n\n\t\t\n\t" +
-        "</head>\n\t" +
-        "<body>\n\t\n\t" +
-        "</body>\n" +
-        "</html>";
-
-    src = baseTemplate.replace('</body>', html + '</body>');
-
-    css = '<style>' + css + '</style>';
-    src = src.replace('</head>', css + '</head>');
-
-    js = '<script>' + js + '</script>';
-    src = src.replace('</body>', js + '</body>');
-
-    return src;
-};
-function render() {
-    var source = prepareSource();
-
-    var iframe = document.querySelector('.output iframe'),
-        iframe_doc = iframe.contentDocument;
-
-    iframe_doc.open();
-    iframe_doc.write(source);
-    iframe_doc.close();
-};
-
-function preset(presetHTML, presetCSS, presetJS) {
-    if(presetHTML)
-        htmlEditor.value += presetHTML;
-    if(presetCSS)
-        cssEditor.value += presetCSS;
-    if(presetJS)
-        jsEditor.value += presetJS;
-    render();
-}
