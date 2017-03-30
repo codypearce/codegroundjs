759fff4a9d2a2c473acf19b2d057f32c3c8ebdf7
codypearce <outis161@gmail.com>
2017-03-29 19:19:48 -0600

    add codeground

---
+++ script.js
@@ -0,0 +1,22 @@
+// var opts = {
+//     html: '<div id="clouds"><div class="cloud"></div></div>', // add preset html, css, js by adding a path to the file you want
+//     css: '#clouds,body,html{height:100%;width:100%}*{margin:0;padding:0}body,html{overflow:hidden}#clouds{padding:100px 0;background:#00e4ff;z-index:-1}.cloud{width:200px;height:83px;background:#fff;border-radius:200px;position:relative;z-index:0;margin:20px auto}.cloud:after,.cloud:before{content:"";background:#fff;position:absolute;border-radius:100px;transform:rotate(30deg)}.cloud:before{width:150px;height:80px;top:-15px;left:10px}.cloud:after{width:120px;height:120px;top:-55px;left:auto;right:15px}',
+//     js:  'console.log("test")',
+//     
+//     htmlShow: true, // setting these to false hides that languages editor
+//     cssShow: true, // if set false on tabs it hides the button
+//     jsShow: true,
+// 
+//     layout: 'half', // whether editor/output takes up full width or half
+//     initialFull: 'editors', // this sets the initial view if you choose layout full
+// 
+//     style: 'rows', // Toggle between rows and tabs view
+//     initialTab: 'html', // if tabs are selected this selects the initial shown tab
+//     
+//     fullscreen: false, // html, body, and container must be 100% also
+// 
+//     topbar: true, // this sets a top bar, required for the tabs view
+//     title: 'Codeground' // optional title for the tab
+// }
+// 
+// var codeground = new Codeground('codeground', opts);
