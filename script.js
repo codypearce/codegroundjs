134a3726ab2fe850df387b217c51aa39c7ec3235
codypearce <outis161@gmail.com>
2017-04-02 10:17:41 -0600

    add demo content for codeground

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+var css = '#clouds,body,html{height:100%;width:100%}*{margin:0;padding:0}body,html{overflow:hidden}#clouds{padding:100px 0;background:#00e4ff;z-index:-1}.cloud{width:200px;height:83px;background:#fff;border-radius:200px;position:relative;z-index:0;margin:20px auto}.cloud:after,.cloud:before{content:"";background:#fff;position:absolute;border-radius:100px;transform:rotate(30deg)}.cloud:before{width:150px;height:80px;top:-15px;left:10px}.cloud:after{width:120px;height:120px;top:-55px;left:auto;right:15px}';
 var opts = {
-    htmlStr: '<div>foo</div>',
-    cssStr: '* { color: black}',
-    jsStr: 'console.log("test")', 
+    htmlStr: '<div id="clouds"><div class="cloud"></div></div>',
+    cssStr: css,
+    jsStr: 'console.log("This is working!")', 
     htmlShow: true,
     cssShow: true,
     jsShow: true,
