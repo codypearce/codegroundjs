2c6dbb6909890506fbda4fd98c57b04017c5ecfa
codypearce <outis161@gmail.com>
2017-04-21 19:59:11 -0600

    css spacing

--- demo/script.js
+++ demo/script.js
@@ -1,4 +1,4 @@
-var css = '#clouds,body,html{height:100%;width:100%}*{margin:0;padding:0}body,html{overflow:hidden}#clouds{padding:100px 0;background:#00e4ff;z-index:-1}.cloud{width:200px;height:83px;background:#fff;border-radius:200px;position:relative;z-index:0;margin:20px auto}.cloud:after,.cloud:before{content:"";background:#fff;position:absolute;border-radius:100px;transform:rotate(30deg)}.cloud:before{width:150px;height:80px;top:-15px;left:10px}.cloud:after{width:120px;height:120px;top:-55px;left:auto;right:15px}';
+var css = '#clouds,body,html{height:100%;width:100%}\n *{margin:0;padding:0} \n body,html{overflow:hidden} \n #clouds{padding:100px 0;background:#00e4ff;z-index:-1} \n.cloud{width:200px;height:83px;background:#fff;border-radius:200px;position:relative;z-index:0;margin:20px auto} \n.cloud:after,.cloud:before{content:"";background:#fff;position:absolute;border-radius:100px;transform:rotate(30deg)} \n.cloud:before{width:150px;height:80px;top:-15px;left:10px} \n.cloud:after{width:120px;height:120px;top:-55px;left:auto;right:15px}';
 var opts = {
     htmlStr: '<div id="clouds">\n \t<div class="cloud"></div> \n</div>',
     cssStr: css,
